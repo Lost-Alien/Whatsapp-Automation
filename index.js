445fb94aeceeee005653661cebba8450343c8775
Lost-Alien <lostdev.tech@gmail.com>
2026-08-07 12:26:53 +0530

    Fix puppeteer execution context timeouts

--- index.js
+++ index.js
@@ -32,9 +32,15 @@ let client = null; // Will be initialized in startClient()
 function createClient() {
     return new Client({
         authStrategy: new LocalAuth({ dataPath: './.wwebjs_auth' }),
+        authTimeoutMs: 120000,
+        webVersionCache: {
+            type: 'remote',
+            remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+        },
         puppeteer: {
             headless: true,
             executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
+            timeout: 120000,
             args: [
                 '--no-sandbox',
                 '--disable-setuid-sandbox',
